915cdb98cd4a4fdd814a9e5b81d38ae16e3b021f
aloksingh96 <aloklsingh96@gmail.com>
2019-01-11 12:55:19 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+
+Future<http.Response> callapi() {
+ var response = 'https://jsonplaceholder.typicode.com/posts';
+
+ print("Response status: ${response}");
+ //print("Response body: ${response.body}");
+}
+}
+
